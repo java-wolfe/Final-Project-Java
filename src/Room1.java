fc20b50027cc1e294cc9cd6dbfe82d3835be9ea3
DanielM14 <37811239+DanielM14@users.noreply.github.com>
2019-10-14 09:49:32 -0500

    update: Room one

--- src/Room1.java
+++ src/Room1.java
@@ -1,2 +1,17 @@
+import java.util.Scanner;
+import java.util.Random;
+
 public class Room1 {
+
+   public static void storyForRoomOne(){
+
+       Scanner input = new Scanner(System.in);
+       Random rand = new Random();
+
+       int userChoice;
+       int rand_pick = rand.nextInt(5) + 1;
+
+
+   }
+   
 }
