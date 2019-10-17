a4d4f4a9dc32f883ae44b9dd41652b783b8b2aed
frantz2027 <54542735+frantz2027@users.noreply.github.com>
2019-10-17 08:59:00 -0500

    Add files via upload

--- src/Graphics.java
+++ src/Graphics.java
@@ -75,5 +75,44 @@ public class Graphics {
 
 
     }
+    public static void defaultHospital(){
+        System.out.println("--------------------------------------------------");
+        System.out.println("                                                  ");
+        System.out.println("                                                  ");
+        System.out.println("                                                  ");
+        System.out.println("-----------------|              |-----------------");
+        System.out.println("                 |       0      |");
+        System.out.println("                 |      -|-     |");
+        System.out.println("                 |      | |     |");
+        System.out.println("                 |              |");
+        System.out.println("                 |              |");
+
+    }
+    public static void hospitalLeft(){
+        System.out.println("--------------------------------------------------");
+        System.out.println("      0                                           ");
+        System.out.println("     -|-                                          ");
+        System.out.println("     | |                                          ");
+        System.out.println("-----------------|              |-----------------");
+        System.out.println("                 |              |");
+        System.out.println("                 |              |");
+        System.out.println("                 |              |");
+        System.out.println("                 |              |");
+        System.out.println("                 |              |");
+
+
+    }
+    public static void hospitalRight(){
+        System.out.println("--------------------------------------------------");
+        System.out.println("                                            0     ");
+        System.out.println("                                           -|-    ");
+        System.out.println("                                           | |    ");
+        System.out.println("-----------------|              |-----------------");
+        System.out.println("                 |              |");
+        System.out.println("                 |              |");
+        System.out.println("                 |              |");
+        System.out.println("                 |              |");
+        System.out.println("                 |              |");
+    }
 
 }
